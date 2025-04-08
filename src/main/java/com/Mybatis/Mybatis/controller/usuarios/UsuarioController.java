ed29639c0255c923c61981d727aa7f8dbb38cbfb
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-04-08 12:57:10 -0600

    configuracion v1.2.5

--- src/main/java/com/Mybatis/Mybatis/controller/usuarios/UsuarioController.java
+++ src/main/java/com/Mybatis/Mybatis/controller/usuarios/UsuarioController.java
@@ -3,6 +3,7 @@ package com.Mybatis.Mybatis.controller.usuarios;
 import com.Mybatis.Mybatis.module.entities.Usuario;
 import com.Mybatis.Mybatis.module.repository.RepositoryUsuario;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -30,25 +31,37 @@ public class UsuarioController {
 
     // Insertar nuevo usuario
     @PostMapping
-    public ResponseEntity<Usuario> insertUsuario(@RequestBody Usuario usuario) {
-        repositoryUsuario.insertUsuario(usuario);
-        return ResponseEntity.ok(usuario);  // Retorna el usuario con id actualizado
+    public ResponseEntity<?> insertUsuario(@RequestBody Usuario usuario) {
+        try {
+            repositoryUsuario.insertUsuario(usuario);
+            return ResponseEntity.ok(usuario);  // Retorna el usuario con ID actualizado
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al insertar el usuario: " + e.getMessage());
+        }
     }
 
     // Actualizar usuario
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> updateUsuario(@PathVariable long id, @RequestBody Usuario usuario) {
-        usuario.setId(id);  // Aseguramos que el ID sea el correcto
-        repositoryUsuario.updateUsuario(usuario);
-        return ResponseEntity.ok(usuario);  // Retorna el usuario actualizado
+    public ResponseEntity<?> updateUsuario(@PathVariable long id, @RequestBody Usuario usuario) {
+        try {
+            usuario.setId(id);
+            repositoryUsuario.updateUsuario(usuario);
+            return ResponseEntity.ok(usuario);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al actualizar el usuario: " + e.getMessage());
+        }
     }
 
 
     // Eliminar usuario
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUsuario(@PathVariable long id) {  // Cambiado a long
-        repositoryUsuario.deleteUsuarioById(id);
-        return ResponseEntity.ok("Usuario eliminado exitosamente.");
+    public ResponseEntity<?> deleteUsuario(@PathVariable long id) {
+        try {
+            repositoryUsuario.deleteUsuarioById(id);
+            return ResponseEntity.ok().body("Usuario eliminado exitosamente.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar el usuario: " + e.getMessage());
+        }
     }
 }
 
