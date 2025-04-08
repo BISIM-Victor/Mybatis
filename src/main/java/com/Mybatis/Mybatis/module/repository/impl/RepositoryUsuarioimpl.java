9abaa6bc94f5456669ca8ba56d30b0a3ba0b653c
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-04-08 13:32:51 -0600

    configuracion v1.2.6

--- src/main/java/com/Mybatis/Mybatis/module/repository/impl/RepositoryUsuarioimpl.java
+++ src/main/java/com/Mybatis/Mybatis/module/repository/impl/RepositoryUsuarioimpl.java
@@ -5,39 +5,48 @@ import com.Mybatis.Mybatis.module.entities.Usuario;
 import com.Mybatis.Mybatis.module.repository.RepositoryUsuario;
 import org.springframework.stereotype.Repository;
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 @Repository("repositoryUsuario")
 public class RepositoryUsuarioimpl implements RepositoryUsuario {
 
+    // Inyectar el UsuarioMapper para ejecutar las consultas de MyBatis
     @Resource
-    private UsuarioService usuarioService;
+    private RepositoryUsuario  usuarioMapper;
 
     @Override
     public List<Usuario> getAllUsuarios() {
-        return usuarioService.getAllUsuarios();
+        // Llamar al método de MyBatis para obtener todos los usuarios
+        return usuarioMapper.getAllUsuarios();
     }
 
     @Override
-    public Usuario getUsuarioById(long id) {  // Cambiado a long
-        return usuarioService.getUsuarioById(id);
+    public Usuario getUsuarioById(long id) {
+        // Llamar al método de MyBatis para obtener un usuario por ID
+        return usuarioMapper.getUsuarioById(id);
     }
 
     @Override
     public void insertUsuario(Usuario usuario) {
-        usuarioService.insertUsuario(usuario);
+        // Llamar al método de MyBatis para insertar un nuevo usuario
+        usuarioMapper.insertUsuario(usuario);
     }
 
     @Override
     public void updateUsuario(Usuario usuario) {
-        usuarioService.updateUsuario(usuario);
+        // Llamar al método de MyBatis para actualizar un usuario
+        usuarioMapper.updateUsuario(usuario);
     }
 
     @Override
-    public void deleteUsuarioById(long id) {  // Cambiado a long
-        usuarioService.deleteUsuarioById(id);
+    public void deleteUsuarioById(long id) {
+        // Llamar al método de MyBatis para eliminar un usuario por ID
+        usuarioMapper.deleteUsuarioById(id);
     }
 }
+
+
 
 
 
