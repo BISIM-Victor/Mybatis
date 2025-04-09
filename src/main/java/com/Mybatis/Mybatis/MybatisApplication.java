d34249696bede6323c1d3e6ef8314394cd695db1
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-04-08 20:30:50 -0600

    configuracion v1.2.9

--- src/main/java/com/Mybatis/Mybatis/MybatisApplication.java
+++ src/main/java/com/Mybatis/Mybatis/MybatisApplication.java
@@ -2,9 +2,8 @@ package com.Mybatis.Mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 
-@SpringBootApplication(exclude = { AopAutoConfiguration.class })
+@SpringBootApplication
 public class MybatisApplication {
 
 	public static void main(String[] args) {
